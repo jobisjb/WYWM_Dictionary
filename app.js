77c965e2bc5a6687f4ccdff0b785df5f244e5daf
jobebabou <jobisjb@yahoo.com>
2022-06-16 22:09:53 +0100

    jokes and whatmyip sections added.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const element = document.getElementById("starSigns");
 window.addEventListener("load", loadInpirationQuote());
 window.addEventListener("load", horoscopeToday);
+window.addEventListener("load", loadPublicIP);
+window.addEventListener("load", getJokes);
+
+
 element.addEventListener("click", horoscopeToday);
-
-
 
 
 function loadInpirationQuote() {
@@ -21,8 +23,20 @@ function loadInpirationQuote() {
             document.getElementById("one").innerHTML = text;
         })
 }
-function loadSelectMenu(){
 
+function loadPublicIP(){
+    const url = "https://api.ipify.org/?format=json";
+    let text ="";
+    fetch(url)
+        .then((res) => res.json())
+        .then(function (object) {
+            text += "<h1>" + object.ip + "</h1>"
+            document.getElementById("three").innerHTML = text;
+        })
+        .catch((err) => {
+            text += "No public IP detected...";
+            document.getElementById("three").innerHTML = text;
+        })
 }
 
 function horoscopeToday() {
@@ -51,5 +65,27 @@ function horoscopeToday() {
         .catch((err) => {
             text += `No horoscope information available for ${starSign}, sorry...`;
             document.getElementById("two").innerHTML = text;
+        })
+}
+
+function getJokes(){
+    const jokeUrl = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist,sexist";
+    let text = "";
+    let text2 = "";
+    fetch(jokeUrl)
+        .then((res) => res.json())
+        .then(function (object) {
+            if (object.type == "twopart") {
+                text += "<p><h3>"+ object.setup + "</h3></p>";
+                text2 += "<div class='coverInfo' id ='six'><h4>"+object.delivery+"<h4></div>"
+            }else if(object.type == "single"){
+                text += "<p><h3>"+ object.joke + "</h3></p>";
+            }
+            document.getElementById("four").innerHTML = text;
+            document.getElementById("five").innerHTML = text2;
+        })
+        .catch((err) => {
+            text += "No jokes to deliver...";
+            document.getElementById("four").innerHTML = text;
         })
 }
